f93b1c32030a7b0dedda14857f9180eda9a0e8c1
LukaG <32247945+LukaGlisic@users.noreply.github.com>
2021-05-23 19:12:35 +0200

    Update skripta.py

--- Elektricno polje/skripta.py
+++ Elektricno polje/skripta.py
@@ -1,20 +1,17 @@
-
 import matplotlib 
 from numpy import *
 from pylab import *
-from fileinput import input
 
-x=linspace(-2, 2, 32)
-y=linspace(-1.5, 1.5, 24)
+x=linspace(-2, 2, 40)
+y=linspace(-2, 2, 40)
 x, y=meshgrid(x, y)
 
  
 def E(q, a, x, y):
-    return q*(x-a[0])/((x-a[0])**2+(y-a[1])**2)**(1.5), \
-        q*(y-a[1])/((x-a[0])**2+(y-a[1])**2)**(1.5)
- 
+    return q*(x-a[0])/((x-a[0])**2+(y-a[1])**2), \
+        q*(y-a[1])/((x-a[0])**2+(y-a[1])**2)
 # racunjanje vektora
-Ex1, Ey1=E(2, [-1, 0], x, y)
+Ex1, Ey1=E(5, [-1, 0], x, y)
 Ex2, Ey2=E(5, [1, 0], x, y)
 Ex=Ex1+Ex2
 Ey=Ey1+Ey2
